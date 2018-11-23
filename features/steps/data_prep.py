3e0a9e239f0a227ab1cf6c2b74e8afd5614a1cd8
Wei Ji <weiji1994@Hotmail.com>
2018-11-23 13:51:17 +1300

    :white_check_mark: Add tests for 'grid datasets' feature
    Checks that the full processing pipeline works. Covers processing from raw text file to clean xyz to interpolation to output file opening. Modifies 19784f8ca53c2966502ce5fb6938a128cfbe5af8.

--- features/steps/data_prep.py
+++ features/steps/data_prep.py
@@ -20,33 +20,36 @@ def check_sha256_of_file(context, sha256):
     os.remove(path=context.filepath)  # remove downloaded file
 
 
-@given("a high resolution dataset {input_file}")
-def collection_of_high_resolution_datasets(context, input_file):
-    #!cd highres && ls *.json
-    dataframe = context.data_prep.parse_datalist()  # retrieve from data_list.yml
-    subset_df = dataframe[dataframe.filename == input_file]  # select based on filename
+@given("a collection of raw high resolution datasets {input_pattern}")
+def collection_of_high_resolution_datasets(context, input_pattern):
+    df = context.data_prep.parse_datalist()  # retrieve from data_list.yml
+    subset_df = df[df.filename.str.match(input_pattern)]  # pattern match filename
 
-    context.input_files = []  # setup empty list to store input files
+    context.input_files = []  # setup empty list to store path to downloaded input files
     for file in subset_df.itertuples():
-        context.folder = file.folder
         filepath = os.path.join(file.folder, file.filename)  # join folder and filename
         context.data_prep.download_to_path(path=filepath, url=file.url)  # download
+        assert context.data_prep.check_sha256(path=filepath) == file.sha256
         context.input_files.append(filepath)  # append filepath to the input list
 
 
-@when("we process it through {pipeline_file}")
+@when("we process the data through {pipeline_file}")
 def process_data_through_pipeline_and_get_output(context, pipeline_file):
-    context.output_files = []  # setup empty list to store output files
-    for filepath in context.input_files:
-        pipeline_file = os.path.join(context.folder, pipeline_file)  # prepend dirname
-        outfile = context.data_prep.processing_pipeline(pipeline_file=pipeline_file)
-        context.output_files.append(outfile)  # append filepath to the output list
+    pf = os.path.join("highres", pipeline_file)  # join folder and filename
+    context.xyz_data = context.data_prep.ascii_to_xyz(pipeline_file=pf)
+    assert list(context.xyz_data.columns) == ["x", "y", "z"]
 
 
-@then("a raster grid is returned {output_file}")
-def open_raster_grid_to_check(context, output_file):
-    assert len(context.output_files) == 1  # check that only one raster is produced
-    for outfile in context.output_files:
-        assert outfile == os.path.join(context.folder, output_file)
-        with rasterio.open(outfile) as raster_source:
-            assert raster_source.closed == False  # check that it can be opened
+@when("interpolate the xyz data table to {output_file}")
+def interpolate_xyz_data_to_grid(context, output_file):
+    region = context.data_prep.get_region(context.xyz_data)
+    context.outfile = os.path.join("highres", output_file)
+    context.data_prep.xyz_to_grid(
+        xyz_data=context.xyz_data, region=region, outfile=context.outfile
+    )
+
+
+@then("a high resolution raster grid is returned")
+def open_raster_grid_to_check(context):
+    with rasterio.open(context.outfile) as raster_source:
+        assert raster_source.closed == False  # check that it can be opened
