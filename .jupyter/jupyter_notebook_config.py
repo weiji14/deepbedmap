495986de5895a5e52d9fff5f8ddf95d5540ac7fd
Wei Ji <weiji1994@Hotmail.com>
2018-11-26 16:49:33 +1300

    :heavy_plus_sign: Add jupytext converter
    Allows notebooks to be stored as plain python scripts too! See https://github.com/mwouts/jupytext. Choosing the py:percent format https://github.com/mwouts/jupytext#the-percent-format that works with Hydrogen in the Atom editor and other IDEs. This py:percent configuration is set in the .jupyter/jupyter_notebook_config.py file, which itself is referenced via a JUPYTER_CONFIG_DIR environmental variable set in the .env file.
    
    Once we start pairing .ipynb and .py files, this jupytext setup will allow for nicer git diffs, and hopefully ease development workflows. Will look at introducing linters, proper code coverage possibly, and who knows what else!

---
+++ .jupyter/jupyter_notebook_config.py
@@ -0,0 +1,6 @@
+# Configuration file for jupyter-notebook.
+
+## Jupytext https://github.com/mwouts/jupytext
+
+c.NotebookApp.contents_manager_class = "jupytext.TextFileContentsManager"
+c.ContentsManager.preferred_jupytext_formats_save = "py:percent"
